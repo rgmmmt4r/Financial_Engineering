35be6d524918aca2220b32074691564178881515
Eddie Yen <36756732+rgmmmt4r@users.noreply.github.com>
2022-12-23 19:45:19 +0800

    增加註解

--- binomal/binomial.py
+++ binomal/binomial.py
@@ -7,8 +7,7 @@ n = 10
 fair_value = 0
 for i in range(7,n+1):
     fair_value += math.comb(n, i) *0.5**i*0.5**(n-i)*i
-    #print (math.comb(n, i))
-#print(fair_value)
+    #fair_value 為 投擲統版中，7次正面或以上的機率加總
 
 
 #模擬ST
@@ -19,23 +18,20 @@ T = 0.5
 r = 0.05
 K = 105
 delta_t = T/n 
-u = np.exp(  sigma * np.sqrt(delta_t))
-d = np.exp(  -sigma * np.sqrt(delta_t))
-p = (np.exp( r*delta_t) -d)/(u-d) # u 的 risk neutral機率
-#print("p",p)
+u = np.exp(  sigma * np.sqrt(delta_t))  # u 是上升幅度
+d = np.exp(  -sigma * np.sqrt(delta_t)) # d 是下降幅度
+p = (np.exp( r*delta_t) -d)/(u-d) # p 是 u 的 risk neutral機率
 ST = 0
+
+# 紀錄 ST 以及 CT 的分布
 for i in range(n+1):
-    ST = S0 *  u**i*d**(n-i)
-    #print (math.comb(n, i))
-    #print("ST:", ST)
-    CT = round( max(ST-K,0),2)
-    #print("CT:", CT)
+    ST = S0 *  u**i*d**(n-i) # 計算 u 發生 i 次 d  發生 n - i 次時的ST
+    CT = round( max(ST-K,0),2)  # 計算 u 發生 i 次 d  發生 n - i 次時的CT
 
 
 #計算每個node 的機率
 for i in range(n+1):
     this_node_prob =  round (math.comb(n, i)*(p)**i*(1-p)**(n-i),2)
-    #print(this_node_prob)
 
 
 #計算call 選擇權價格
@@ -45,8 +41,10 @@ for i in range(n+1):
     ST = S0 *  u**i*d**(n-i)
     CT = round( max(ST-K,0),2)
     this_node_prob =  round (math.comb(n, i)*(p)**i*(1-p)**(n-i),2)
+    # call price 為 各個node 中， CT 乘上該node 發燒機率的加權平均
     call_price += CT * this_node_prob
-    pv_call_price = np.exp(-r*T)*call_price 
+    #折現
+    pv_call_price = np.exp(-r*T)*call_price  
 
 #print("pv_call_price:", round( pv_call_price,2))
 
@@ -54,14 +52,14 @@ def binomial_pricing_EU(S0,sigma,T,r,K,n,type):
     delta_t = T/n 
     u = np.exp(  sigma * np.sqrt(delta_t))
     d = np.exp(  -sigma * np.sqrt(delta_t))
-    p = (np.exp( r*delta_t) -d)/(u-d) # u 的 risk neutral機率
-    future_price = 0
+    p = (np.exp( r*delta_t) -d)/(u-d) 
+    
     if type == "call":
         for i in range(n+1):
             ST = S0 *  u**i*d**(n-i)
             CT = Decimal( max(ST-K,0))
             this_node_prob =  Decimal(combin(n,i)*(p)**i*(1-p)**(n-i))
-            future_price += CT * this_node_prob
+            future_price += CT * this_node_prob # future price 為未來價格，尚未折現
     elif  type == "put":
         for i in range(n+1):
             ST = S0 *  u**i*d**(n-i)
